6c3772406aa3a13e2203012752c8e1ba86871b0e
chaotang <ctang84@gatech.edu>
2019-03-04 17:06:51 -0500

    implement imagenet validation w/o dataloaders

--- test.py
+++ test.py
@@ -34,24 +34,33 @@ def map_class_indices():
 
 class_index_dict = map_class_indices()
 
-resnet18 = models.resnet18(pretrained=True).cuda().eval()
+resnet152 = models.resnet152(pretrained=True).cuda().eval()
 mean = np.array([0.485, 0.456, 0.406]).reshape((3,1,1))
 std = np.array([0.229, 0.224, 0.225]).reshape((3,1,1))
-model = foolbox.models.PyTorchModel(resnet18, bounds=(0,1), num_classes=1000, preprocessing=(mean, std))
+model = foolbox.models.PyTorchModel(resnet152, bounds=(0,1), num_classes=1000, preprocessing=(mean, std))
 
 attack = foolbox.attacks.GradientSignAttack(model)
 
 attack_dir = "./attack_dir/"
-image_paths = glob(r'/home/chaotang/Desktop/special_problem/test/**/*.JPEG', recursive=True)
+image_paths = glob(r'/home/chaotang/Desktop/special_problem/ILSVRC2012/val/**/*.JPEG', recursive=True)
+
+count = 0
 
 for image_path in image_paths:
+    print(image_path)
     image = cv2.imread(image_path)
+    image = cv2.resize(image, (224, 224))
     image = (np.transpose(image, (2, 1, 0))/255).astype("float32")
-    image = np.resize(image, (3,224, 224))
+    prediction = np.argmax(model.predictions(image))
     code = os.path.basename(os.path.dirname(image_path))
     label = class_index_dict[code]
-    adv_image = attack(image, label, max_epsilon=0.5, epsilons=1)
-    class_dir = os.path.join(attack_dir, code)
-    if not(os.path.exists(class_dir)):
-        os.mkdir(class_dir)
-    cv2.imwrite(filename=os.path.join(class_dir, os.path.basename(image_path)), img=np.uint8(adv_image*255))
+    if label == prediction:
+        count += 1
+    #adv_image = attack(image, label, max_epsilon=0.5, epsilons=1)
+    # adv_image = image
+    # class_dir = os.path.join(attack_dir, code)
+    # if not(os.path.exists(class_dir)):
+    #     os.mkdir(class_dir)
+    # cv2.imwrite(filename=os.path.join(class_dir, os.path.basename(image_path)), img=np.uint8(np.transpose(adv_image, (2, 1, 0))*255))
+
+print(count/len(image_paths))
