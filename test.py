efa8ee2e8926ec07d5590aa026ca8e7671a12786
chaotang <ctang84@gatech.edu>
2019-03-08 17:04:38 -0500

    Skeleton for single-image diffusion + median filter

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 from glob import glob
 from torchvision import models
 import foolbox
-import torch
-import torch.nn as nn
 import numpy as np
 import os
-import cv2
-from shutil import copy2
 import torchvision.transforms as transforms
 from PIL import Image
+from medpy import filter as filter
+
 
 # Attacked_Dir := root folder of adversarial examples
 # img_paths = glob(path_to_val)
@@ -30,8 +28,8 @@ def load_image(img_path):
     )
 
     preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        #transforms.Resize(256),
+        #transforms.CenterCrop(224),
         transforms.ToTensor(),
         normalize
     ])
@@ -43,6 +41,7 @@ def load_image(img_path):
     img = img.numpy()  # convert back to numpy array
 
     return img
+
 
 
 def map_class_indices():
@@ -68,18 +67,18 @@ class_index_dict = map_class_indices()
 resnet152 = models.resnet152(pretrained=True).cuda().eval()
 model = foolbox.models.PyTorchModel(resnet152, bounds=(0.0, 1.0), num_classes=1000, preprocessing=(0, 1))
 
-attack = foolbox.attacks.GradientSignAttack(model)
-attack_dir = "./attack_dir/"
-
-image_paths = glob(r'/media/fantasie/backup/data/ILSVRC2012/val/**/*.JPEG', recursive=True)
-target_dir = '/media/fantasie/backup/data/ILSVRC2012/val_correct'
+image_paths = glob(r'./attack_test/**/*.JPEG', recursive=True)
 
 i, count = 1, 0
 
 for image_path in image_paths:
-    print("i = %d" % i); i += 1
+    print("i = %d" % i)
+    i += 1
 
     image = load_image(image_path)
+    #image = medfilt(image).astype("float32")
+    image = filter.anisotropic_diffusion(image).astype("float32")
+
     prediction = np.argmax(model.predictions(image))
 
     code = os.path.basename(os.path.dirname(image_path))
@@ -87,20 +86,5 @@ for image_path in image_paths:
 
     if label == prediction:
         count += 1
-        print("count = %d" % count)
-
-        parts = image_path.split('/')
-        output_dir = os.path.join(target_dir, parts[-2])
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-        copy2(image_path, output_dir)
-
-    #adv_image = attack(image, label, max_epsilon=0.5, epsilons=1)
-    # adv_image = image
-    # class_dir = os.path.join(attack_dir, code)
-    # if not(os.path.exists(class_dir)):
-    #     os.mkdir(class_dir)
-    # cv2.imwrite(filename=os.path.join(class_dir, os.path.basename(image_path)), img=np.uint8(np.transpose(adv_image, (2, 1, 0))*255))
 
 print("Test accuracy: ", count/len(image_paths))
