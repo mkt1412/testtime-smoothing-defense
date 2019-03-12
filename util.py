776f942c785f2e4850f5677bff4fc98ff24f9a0b
thelittlekid <yifeioffice@gmail.com>
2019-03-11 21:00:00 -0400

    Pre-compute and save adversarial examples with IBM-ART (faster)
    The utility function load_image works in multiple modes, and a user can
    specify whether or not to resize or normalize using boolean flags.
    Also migrate select-correctly-classified-samples to utility.

--- util.py
+++ util.py
@@ -5,15 +5,25 @@ from matplotlib import pyplot as plt
 import os
 from glob import glob
 import foolbox
+from art.classifiers import PyTorchClassifier
+from art.attacks.projected_gradient_descent import ProjectedGradientDescent
+from art.attacks.fast_gradient import FastGradientMethod
+import torch
 from torchvision import models
 import cv2
 import getpass
 
 # Set up the root directory of ImageNet
 if getpass.getuser() == 'fantasie':  # user is Yifei
-    ROOT_DIR = '/media/fantasie/backup/data/ILSVRC2012/val_correct'
+    ROOT_DIR = '/media/fantasie/backup/data/ILSVRC2012/'  # root directory for ImageNet
+    SOURCE_DIR = ROOT_DIR + 'val/'  # directory for validation set
+    CORRECT_DIR = ROOT_DIR + 'val_correct1/'  # directory for correctly classified validation samples
+    ADV_DIR = ROOT_DIR + 'val_correct_adv_resnet152_fast-gradient/'  # directory for precomputed adversarial examples
 else:  # user is Chao
     ROOT_DIR = ''
+    SOURCE_DIR = ROOT_DIR + 'val'
+    CORRECT_DIR = ROOT_DIR + 'val_correct'
+    ADV_DIR = ROOT_DIR + ''
 
 
 def display_array_as_image(arr):
@@ -29,25 +39,64 @@ def display_array_as_image(arr):
     plt.imshow(arr.astype(np.uint8))
 
 
-def load_image(img_path):
+def load_image(img_path, resize=True, normalize=False):
     """
     Load image from a specified file path
     :param img_path: the file path of the image
-    :return: numpy array with shape (h x w x 3)
+    :param resize: whether or not to perform resize and centercrop, either do it here or with the model
+    :param normalize: whether or not to perform normalization, either do it here or with the model
+    :return: float32 numpy array with shape (3 x h x w)
     """
-    # Standard pytorch preprocessing
-    preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-    ])
-
     img = Image.open(img_path)
     if img.mode != 'RGB':  # convert other modes with different number of channels to 3-channel'RGB'
         img = img.convert(mode='RGB')
-    img = preprocess(img)
-    img = np.transpose(np.array(img), (2, 0, 1)) / 255.0  # channel first and [0, 1]
 
-    return img.astype(np.float32)
+    if resize:
+        preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+        ])
+        img = preprocess(img)
+
+    if normalize:
+        normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),  # converts to channel-first tensor
+            normalize,
+        ])
+        img = np.array(preprocess(img))
+    else:
+        img = np.transpose(np.array(img), (2, 0, 1)).astype(np.float32) / 255.0  # channel first and [0, 1]
+
+    return img
+
+
+def load_image_batch(img_paths):
+    """
+    Load a batch of images
+    :param img_paths: file paths of the images in the batch
+    :return: numpy array with size (N x 3 x h x w)
+    """
+    imgs = []
+    for img_path in img_paths:
+        imgs.append(load_image(img_path))
+
+    return np.stack(imgs, axis=0)
+
+
+def numpy_to_tensor(arr):
+    """
+    Convert numpy-array image to a Pytorch tensor
+    :param arr: numpy array representing an image, size (3 x h x w)
+    :return: Pytorch tensor with size (None x 3 x h x w)
+    """
+    if len(arr.shape) < 4:
+        arr = np.expand_dims(arr, axis=0)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    return torch.from_numpy(arr).to(device)
 
 
 def map_class_indices():
@@ -66,6 +115,47 @@ def map_class_indices():
         count += 1
 
     return class_index
+
+
+def select_correct_images(source_dir=SOURCE_DIR, target_dir=CORRECT_DIR, save_files=False):
+    """
+    Scripts used for selecting samples that are correctly classified by a pretrained network
+    :param source_dir: source directory that contains the universe of test images
+    :param target_dir: target directory that used for saving correctly classified images when save_files is True
+    :param save_files: indicator: whether or not to copy and store the correctly classified images
+    :return: None
+    """
+    from shutil import copy2
+    # Configure the pretrained model
+    model = models.resnet152(pretrained=True).cuda().eval()
+
+    image_paths = sorted(glob(os.path.join(source_dir + '**/*.JPEG'), recursive=True))
+
+    i, count = 1, 0
+    class_index_dict = map_class_indices()  # {n01440764: 0}
+    for image_path in image_paths:
+        print("i = %d" % i)
+        i += 1
+
+        image = load_image(image_path, True, True)
+        prediction = torch.argmax(model(numpy_to_tensor(image)))
+
+        code = os.path.basename(os.path.dirname(image_path))
+        label = class_index_dict[code]
+
+        if label == prediction:
+            count += 1
+            print("count = %d" % count)
+
+            if save_files:
+                parts = image_path.split('/')
+                output_dir = os.path.join(target_dir, parts[-2])
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+
+                copy2(image_path, output_dir)
+
+    print("Test accuracy: ", count / len(image_paths))
 
 
 def save_array_to_image(arr, fp):
@@ -88,25 +178,29 @@ def save_array_to_image(arr, fp):
 
 def save_adversarial_examples(source_dir):
     """
-    Scripts to compute and save adversarial examples. Need to manually set the models, directories and hyper-parameters
+    Scripts to compute and save adversarial examples with foolbox
+    Need to manually set the models, directories and hyper-parameters
+    Note:
+    Discarded because the execution time is too long for sophisticated attacks such as PGD, and no clipping is provided
+    to account for quantization
     :param source_dir: root directory of the legitimate clean data
     :return: None
     """
     root_dir = os.path.dirname(source_dir)
-    sub_dir = os.path.basename(source_dir) + "_adv_resnet152_gradientsign"  # named after hyper-parameters
+    sub_dir = os.path.basename(source_dir) + "_adv_resnet152_test"  # named after hyper-parameters
     target_dir = os.path.join(root_dir, sub_dir)
 
     # Load and create an instance of pretrained model
-    resnet152 = models.resnet152(pretrained=True).cuda().eval()
+    resnet = models.resnet152(pretrained=True).cuda().eval()
     mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
     std = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
-    model = foolbox.models.PyTorchModel(resnet152, bounds=(0.0, 1.0), num_classes=1000, preprocessing=(mean, std))
+    model = foolbox.models.PyTorchModel(resnet, bounds=(0.0, 1.0), num_classes=1000, preprocessing=(mean, std))
 
     class_index_dict = map_class_indices()  # {n01440764: 0}
 
     image_paths = sorted(glob(source_dir + '/**/*.JPEG', recursive=True))
     count = 0
-    for image_path in image_paths:
+    for image_path in image_paths[0:50]:
         # Get source image and label
         image = load_image(image_path)
         code = os.path.basename(os.path.dirname(image_path))
@@ -124,5 +218,41 @@ def save_adversarial_examples(source_dir):
         print(count)
 
 
+def save_adversarial_examples_batch(source_dir):
+    batch_size = 16
+    root_dir = os.path.dirname(source_dir)
+    sub_dir = os.path.basename(source_dir) + "_adv_resnet152_pgd"  # named after hyper-parameters
+    target_dir = os.path.join(root_dir, sub_dir)
+
+    # Load pretrained model
+    model = models.resnet152(pretrained=True).cuda().eval()
+    # Build crafter for adversarial examples
+    mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
+    std = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
+    classifier = PyTorchClassifier((0.0, 1.0), model=model, preprocessing=(mean, std),
+                                   loss=torch.nn.modules.loss.CrossEntropyLoss(),
+                                   optimizer=torch.optim.Adam,
+                                   input_shape=(3, 224, 224), nb_classes=1000)
+    adv_crafter = ProjectedGradientDescent(classifier)
+
+    image_paths = sorted(glob(ROOT_DIR + '/**/*.JPEG', recursive=True))
+    count = 0
+    for start in range(0, len(image_paths), batch_size):
+        end = min(start + batch_size, len(image_paths))
+        image_batch_paths = image_paths[start:end]
+        images = load_image_batch(image_batch_paths)
+        adv_images = adv_crafter.generate(x=images)
+
+        assert len(adv_images) == len(images)
+
+        print("start: %d, end: %d" % (start, end))
+        for (image_path, adv_image) in zip(image_batch_paths, adv_images):
+            output_path = os.path.join(target_dir, image_path[len(source_dir) + 1:])
+            save_array_to_image(adv_image, output_path)
+
+            count += 1
+            print(count)
+
+
 if __name__ == "__main__":
-    save_adversarial_examples(ROOT_DIR)
+    save_adversarial_examples_batch(ROOT_DIR)
