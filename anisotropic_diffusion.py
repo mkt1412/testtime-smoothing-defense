e16b44ef8a0d5c4c91c0bc96ea0db1bdd00a9c29
chao <ctang84@gatech.edu>
2019-04-09 22:28:31 -0400

    Fix anisotropic diffusion.

--- anisotropic_diffusion.py
+++ anisotropic_diffusion.py
@@ -21,18 +21,21 @@ def anisotropic_diffusion_1d(input, niter=1, kappa=50, gamma=0.1, option=1):
 
     for i in range(niter):
 
-        Ix, Iy = np.gradient(input)
+        Ix, Iy = np.gradient(output)
         Ixx = np.gradient(Ix, axis=0)
         Iyy = np.gradient(Iy, axis=1)
         if option == 1:
-            C = 1. / (1. + (np.sqrt(Ix ** 2 + Iy ** 2) / kappa) ** 2)
+            C = 1 / (1 + (np.sqrt(Ix ** 2 + Iy ** 2) / kappa) ** 2)
         elif option == 2:
-            C = np.exp(-(np.sqrt(Ix**2 + Iy**2)/kappa**2))
+            C = np.exp(-((np.sqrt(Ix ** 2 + Iy ** 2) / kappa) ** 2))
+        elif option == 3:
+            C = 1 / 1 + (np.exp(np.sqrt(Ix**2 + Iy**2) - 20) * np.sqrt(Ix**2 + Iy**2)) ** 2
         else:
             warnings.warn("no such diffusion option")
             quit()
         Cx, Cy = np.gradient(C)
         It = Cx * Ix + Cy * Iy + C * (Ixx + Iyy)
+
         output = output + gamma * It
 
     return output
@@ -65,14 +68,14 @@ def anisotropic_diffusion_3d(input, niter=1, kappa=50, gamma=0.1, option=1):
 
 
 if __name__ == "__main__":
-    img_path = "/home/chao/PycharmProjects/data/ILSVRC2012/" \
-               "val_correct_adv_resnet152_pgd/n01514668/ILSVRC2012_val_00023551.JPEG"
+    img_path = "/home/chao/PycharmProjects/data/ILSVRC2012/val_correct_adv_resnet152_pgd-0.05-0.01-10/n01685808/" \
+               "ILSVRC2012_val_00016379.JPEG"
     raw_image = load_image(img_path=img_path, normalize=False, resize=False)
     plt.ion()
-    for i in range(20):
-        image = anisotropic_diffusion_3d(input=raw_image, niter=i, kappa=50, option=1)
-        # image = anisotropic_diffusion(raw_image, niter=1, gamma=0.1, kappa=10)
-        # image = convolve(raw_image, weights=np.full((1, 3, 3), 1.0 / 9))
+    for i in range(50):
+        image = anisotropic_diffusion_3d(input=raw_image, niter=i, option=1, kappa=50, gamma=0.8)
+        # image = anisotropic_diffusion(raw_image, niter=i, gamma=0.1, kappa=50)
+        # # image = convolve(raw_image, weights=np.full((1, 3, 3), 1.0 / 9))
         image = np.transpose(image, (1, 2, 0))
         plt.figure(1)
         plt.title("step = {}".format(i))
