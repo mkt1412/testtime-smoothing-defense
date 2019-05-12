53b8a301b2139ec3043fcf134538f0ddcfc0c9e8
thelittlekid <yifeioffice@gmail.com>
2019-05-12 10:34:15 -0700

    Revise approximation of 2nd-order derivatives in modified curvature motion
    It might not be appropriate to use central difference again on first-order
    derivatives to compute the second-order derivatives.
    Reference: http://www.mathematik.uni-dortmund.de/~kuzmin/cfdintro/lecture4.pdf

--- modified_curvature_motion.py
+++ modified_curvature_motion.py
@@ -3,6 +3,31 @@ from util import load_image
 from matplotlib import pyplot as plt
 
 
+def shift_image(img, shift, axis=None):
+    """
+    Shift the image according to the specified stepsize and pad it with edge pixels
+    """
+    out = np.roll(img, shift, axis=axis)
+    if shift == 1:
+        if axis == 0:
+            out[0, :, :] = out[1, :, :]
+        elif axis == 1:
+            out[:, 0, :] = out[:, 1, :]
+        else:
+            out[:, :, 0] = out[:, :, 1]
+    elif shift == -1:
+        if axis == 0:
+            out[-1, :, :] = out[-2, :, :]
+        elif axis == 1:
+            out[:, -1, :] = out[:, -2, :]
+        else:
+            out[:, :, -1] = out[:, :, -2]
+    else:
+        return img
+
+    return out
+
+
 def modified_curvature_motion(image, niter=1, k=0.05):
     """
     Implementation of Modified Curvature Motion
@@ -16,11 +41,33 @@ def modified_curvature_motion(image, niter=1, k=0.05):
         image = image / 255.0
 
     for i in range(niter):
-        # compute gradients
+        # Compute first-order gradients
         Ix, Iy, Iz = np.gradient(image)
-        Ixx, Ixy, Ixz = np.gradient(Ix)
-        _, Iyy, Iyz = np.gradient(Iy)
-        Izz = np.gradient(Iz, axis=2)
+
+        # Compute second-order derivatives
+        # Reference: http://www.mathematik.uni-dortmund.de/~kuzmin/cfdintro/lecture4.pdf
+        Ipx, Imx = shift_image(image, -1, 0), shift_image(image, 1, 0)
+        Ipy, Imy = shift_image(image, -1, 1), shift_image(image, 1, 1)
+        Ipz, Imz = shift_image(image, -1, 2), shift_image(image, 1, 2)
+
+        Ipxpy, Ipxmy = shift_image(Ipx, -1, 1), shift_image(Ipx, 1, 1)
+        Imxpy, Imxmy = shift_image(Imx, -1, 1), shift_image(Imx, 1, 1)
+        Ipxpz, Ipxmz = shift_image(Ipx, -1, 2), shift_image(Ipx, 1, 2)
+        Imxpz, Imxmz = shift_image(Imx, -1, 2), shift_image(Imx, 1, 2)
+        Ipypz, Ipymz = shift_image(Ipy, -1, 2), shift_image(Ipy, 1, 2)
+        Imypz, Imymz = shift_image(Imy, -1, 2), shift_image(Imy, 1, 2)
+
+        Ixx = Ipx - 2 * image + Imx
+        Iyy = Ipy - 2 * image + Imy
+        Izz = Ipz - 2 * image + Imz
+
+        Ixy = (Ipxpy - Ipxmy - Imxpy + Imxmy) / 4
+        Ixz = (Ipxpz - Ipxmz - Imxpz + Imxmz) / 4
+        Iyz = (Ipypz - Ipymz - Imypz + Imymz) / 4
+
+        # Ixx, Ixy, Ixz = np.gradient(Ix)
+        # _, Iyy, Iyz = np.gradient(Iy)
+        # Izz = np.gradient(Iz, axis=2)
 
         Ix2 = Ix ** 2
         Iy2 = Iy ** 2
